d719cf4e9b40ebfdd92e900ce72d329e7a2b5aa2
saqlain-codes <70811314+saqlain455@users.noreply.github.com>
2022-01-18 19:12:26 +0500

    model change

--- modelapi/app.py
+++ modelapi/app.py
@@ -2,7 +2,7 @@ from flask import Flask,jsonify,request
 import pickle
 app = Flask(__name__)
 
-@app.route("/",methods=['GET','POST'])
+@app.route("/",methods=['GET'])
 def tryit():
     args=request.args.get('text')
     print(args)
@@ -11,8 +11,8 @@ def tryit():
 
 @app.route("/predict",methods=['GET','POST'])
 def predict_model():
-    filename='filem.sav'
-    loaded_model=pickle.load(open(filename,'rb'))
+    filename='saqlainbefit.sav'
+    loaded_model=pickle.load(open(r'/Users/mac/Documents/GitHub/modelflask/modelapi/ss.sav','rb'))
     print("arguments")
     ex1=request.args.get('text')
     # ex1=request.form['text']
@@ -29,8 +29,10 @@ def predict_model():
     # return str(loaded_model.predict_proba([ex1]))
     # return d
     # return str(d[5])
+    return jsonify({"anger":str(d[1]),"fear":str(d[2]),"hate":str(d[3]),"insomnia":str(d[4]),"sadness":str(d[5]),"socialMedia":str(d[6])})
+   
 
-    return jsonify({"anger":str(d[1]),"boredom":str(d[2]),"fear":str(d[3]),"hate":str(d[4]),"insomnia":str(d[5]),"sadness":str(d[6])})
+    # return jsonify({"anger":str(d[1]),"boredom":str(d[2]),"fear":str(d[3]),"hate":str(d[4]),"insomnia":str(d[5]),"sadness":str(d[6])})
 
 if __name__ == "__main__":
     app.run()
